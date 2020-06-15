bae528f69e309fa30407be48496f08c40834fbf2
בן זלביגר <ben_selbiger@windowslive.com>
2020-06-15 17:11:00 +0300

    WIP - Control panel & Settings content

--- RGBMasterUWPApp/Pages/SettingsPage.xaml.cs
+++ RGBMasterUWPApp/Pages/SettingsPage.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -26,5 +30,26 @@ namespace RGBMasterUWPApp.Pages
         {
             this.InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/rgb-master-team/RGBMaster"));
+        }
+
+        public string VersionNumber { get; set; }
+
+        private void appVer()
+        {
+
+            string appVersion = string.Format("Version: {0}.{1}.{2}.{3}",
+                        Package.Current.Id.Version.Major,
+                        Package.Current.Id.Version.Minor,
+                        Package.Current.Id.Version.Build,
+                        Package.Current.Id.Version.Revision);
+
+            this.VersionNumber = appVersion;
+        }
+
+
     }
 }
