9f2f07eaea62cd2a60bb99b01e2644dffdcc9faf
Dean Kevorkian <deankevor@gmail.com>
2020-07-18 15:31:27 +0300

    Clean marked code.

--- EffectsExecution/EffectExecutor.cs
+++ EffectsExecution/EffectExecutor.cs
@@ -19,46 +19,11 @@ namespace EffectsExecution
         }
 
         protected IEnumerable<Device> Devices { get; private set; } = new List<Device>();
-        private SemaphoreSlim changeConnectedDevicesSemaphore = new SemaphoreSlim(1, 1);
 
         public void ChangeConnectedDevices(IEnumerable<Device> devices)
         {
-            //var addedDevices = devices.Except(this.Devices);
-            //var removedDevices = this.Devices.Except(devices);
-
-            //await changeConnectedDevicesSemaphore.WaitAsync();
-
-            //await DisconnectDevices(removedDevices);
-            //await ConnectDevices(addedDevices);
-
-            //changeConnectedDevicesSemaphore.Release();
-
-            this.Devices = new List<Device>(devices);
+            Devices = new List<Device>(devices);
         }
-
-        //private async Task DisconnectDevices(IEnumerable<Device> devicesToDisconnect)
-        //{
-        //    var connectionTasks = new List<Task>();
-
-        //    foreach (var removedDevice in devicesToDisconnect)
-        //    {
-        //        connectionTasks.Add(removedDevice.Disconnect());
-        //    }
-
-        //    await Task.WhenAll(connectionTasks);
-        //}
-
-        //private async Task ConnectDevices(IEnumerable<Device> devicesConnect)
-        //{
-        //    var connectionTasks = new List<Task>();
-
-        //    foreach (var removedDevice in devicesConnect)
-        //    {
-        //        connectionTasks.Add(removedDevice.Connect());
-        //    }
-
-        //    await Task.WhenAll(connectionTasks);
-        //}
 
         public async Task Start()
         {
