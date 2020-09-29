7d8165752d49657813905059dbe45584bb73bfdc
Dean Kevorkian <deankevor@gmail.com>
2020-09-29 16:35:57 +0300

    Oops lol

--- Common/DominantDisplayColorEffectMetadata.cs
+++ Common/DominantDisplayColorEffectMetadata.cs
@@ -11,7 +11,7 @@ namespace Common
     public class DominantDisplayColorEffectMetadata : EffectMetadata
     {
 
-        public override string EffectName => "Pointer Sync";
+        public override string EffectName => "Dominant display color sync";
 
         public override string ShortDescription => "Syncs lights with the most dominant color of a selected monitor.";
 
