725b1240365562d34fde65a95fa93156d3c72e01
Dean Kevorkian <deankevor@gmail.com>
2020-10-07 04:26:58 +0300

    Added script section to remove older packages in case they exist, before installing the newer version.

--- RGBMasterWap/SignAndInstallApp.ps1
+++ RGBMasterWap/SignAndInstallApp.ps1
@@ -9,4 +9,13 @@ Import-Certificate -FilePath "RGBMasterWap_2.1.0.0_x64.cer" -CertStoreLocation C
 Write-Output "rgbmaster certificate installed successfully."
 Write-Output "Installing actual RGBMaster app-bundle....."
 
+$package = Get-AppPackage -Name "6442bda9-8e61-4e00-a286-4af1d801567a"
+
+if ($package -ne $null) {
+    $packageFullname = $package.PackageFullName
+    Write-Output "Found older RGBMaster package by the full-name of " $packageFullname
+    Write-Output "Uninstalling old package..."
+    Remove-AppPackage -Package $packageFullname
+}
+
 Add-AppPackage -path "RGBMasterWap_2.1.0.0_x64.msixbundle"
