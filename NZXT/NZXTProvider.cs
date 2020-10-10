0987a746d7c1fb7b34cc367745586e3d0826cdf8
Ben <selbiger10@gmail.com>
2020-10-10 22:34:52 +0300

    Added NZXT provider.

---
+++ NZXT/NZXTProvider.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using NZXT.Devices;
+using NZXTSharp.HuePlus;
+using NZXTSharp.KrakenX;
+using Provider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZXT
+{
+    public class NZXTProvider : BaseProvider
+    {
+        private HuePlus huePlusDevice;
+        private KrakenX krakenXDevice;
+
+        public NZXTProvider() : base(new NZXTProviderMetadata())
+        {
+        }
+        public override Task<List<Device>> Discover()
+        {
+            var nzxtHuePlusDevice = new NZXTHuePlusDevice(ProviderMetadata.ProviderGuid, huePlusDevice);
+            var nzxtKrakenXDevice = new NZXTKrakenXDevice(ProviderMetadata.ProviderGuid, krakenXDevice);
+
+            return Task.FromResult(new List<Device>() { nzxtHuePlusDevice, nzxtKrakenXDevice });
+        }
+
+        protected override Task InternalRegister()
+        {
+            huePlusDevice = new HuePlus();
+            krakenXDevice = new KrakenX();
+            return Task.CompletedTask;
+        }
+
+        protected override Task InternalUnregister()
+        {
+            huePlusDevice.Dispose();
+            krakenXDevice.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
