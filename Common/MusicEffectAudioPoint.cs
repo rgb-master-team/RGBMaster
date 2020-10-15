1d181280302b78ebfbe4f4fb00eb86fe2412724e
Dean Kevorkian <deankevor@gmail.com>
2020-10-15 23:59:54 +0300

    Still refactoring and fixing

--- Common/MusicEffectAudioPoint.cs
+++ Common/MusicEffectAudioPoint.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Utils;
 
 namespace Common
 {
@@ -24,7 +25,7 @@ namespace Common
             set
             {
                 minimumAudioPoint = value;
-                OnPropertyChanged();
+                NotifyPropertyChangedUtils.OnPropertyChanged(PropertyChanged, this);
             }
         }
         public Color Color
@@ -38,14 +39,9 @@ namespace Common
                 if (color != value)
                 {
                     color = value;
-                    OnPropertyChanged();
+                    NotifyPropertyChangedUtils.OnPropertyChanged(PropertyChanged, this);
                 }
             }
-        }
-
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
---
+++ Utils/NotifyPropertyChangedUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Utils
+{
+    public static class NotifyPropertyChangedUtils
+    {
+        public static void OnPropertyChanged(PropertyChangedEventHandler propertyChanged, object sender, [CallerMemberName] string name = null)
+        {
+            propertyChanged?.Invoke(sender, new PropertyChangedEventArgs(name));
+        }
+    }
+}
